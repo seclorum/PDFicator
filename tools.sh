4ae3c4e760465baa183f855f3928bce3d16d8a6f
Jay Vaughan <seclorum@mac.com>
2025-01-22 17:54:21 +0100

    tools: huggingface_hub and sentence-transformers need to be pinned

--- tools.sh
+++ tools.sh
@@ -11,19 +11,18 @@ echo "Setting up dependencies..."
 # !J! Only do these if you don't already have your python eggs in order
 #sudo apt-get update -y || brew update
 #sudo apt-get install -y python3.11 python3.11-venv python3.11-dev sqlite3 poppler-utils || brew install python3 sqlite3 poppler
-
-# Create and activate a virtual environment using Python 3.11
+ 
+# Create and activate a virtual environment
 python3.11 -m venv pdf_tool_env
 source pdf_tool_env/bin/activate
 
-# Upgrade pip to the latest version
+# Upgrade pip
 pip install --upgrade pip
 
-# Install specific compatible versions of sentence-transformers and torch for Python 3.11+
-pip install torch==2.0.0+cpu torchvision==0.15.1+cpu torchaudio==2.0.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip install sentence-transformers==2.2.2
-
-# Install other required Python libraries
+# Pin NumPy to a version below 2.0
+pip install "numpy<2"
+pip install torch torchvision torchaudio -f https://download.pytorch.org/whl/cpu
+pip install sentence-transformers==2.2.2 huggingface_hub==0.4.0
 pip install PyPDF2 pdfminer.six pytesseract transformers whoosh scikit-learn faiss-cpu
 
 echo "Dependencies successfully set up!"
